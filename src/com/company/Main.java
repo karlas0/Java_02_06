20cb61be5e55fe9851ac1ed66c8d171b439076d0
Karlas <karolis.zagarnauskas@gmail.com>
2018-01-30 16:20:18 +0200

    first commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,24 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        System.out.println("Sveiki, iveskite savo svori kg");
+
+        Scanner sc = new Scanner(System.in);
+
+        float a = sc.nextFloat();
+
+        System.out.println("Iveskite iveskite savo ugi metrais");
+
+        float b = sc.nextFloat();
+
+        System.out.println("Jusu KMI");
+
+        System.out.println(a / (b * b));
     }
+
 }
